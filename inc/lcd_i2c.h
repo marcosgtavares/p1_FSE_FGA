a090f4e222164b6b0fbd084f97ec1d057a386aff
Marcos Gabriel Tavares <marcosgabriel.ro@hotmail.com>
2021-03-18 11:55:16 -0400

    i2c lcd controller

---
+++ inc/lcd_i2c.h
@@ -0,0 +1,16 @@
+
+#ifndef LCD_I2C_H_
+#define LCD_I2C_H_
+
+void lcd_init(void);
+void lcd_byte(int bits, int mode);
+void lcd_toggle_enable(int bits);
+void typeInt(int i);
+void typeFloat(float myFloat);
+void lcdLoc(int line); //move cursor
+void ClrLcd(void); // clr LCD return home
+void typeln(const char *s);
+void typeChar(char val);
+void set_i2c_addr_lcd();
+
+#endif /* LCD_I2C_H_ */
