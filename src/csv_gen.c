8af13edd29e7462a3d67fcf6830196cd5d8d5396
Marcos Gabriel Tavares <marcosgabriel.ro@hotmail.com>
2021-03-18 23:53:41 -0400

    no more strcat

--- src/csv_gen.c
+++ src/csv_gen.c
@@ -5,7 +5,7 @@
 FILE *fp;
 void create_csv(char *d_h, double ti, double te, double tr, double res, double fan){
     if(ti==0 && te==0 && tr==0 && res==0 && fan==0){
-        char *filename, temp[23];
+        char temp[23];
         int i=0;
         while(d_h[i+9]!='\0'){
             temp[i]=d_h[i+4];
@@ -13,21 +13,21 @@ void create_csv(char *d_h, double ti, double te, double tr, double res, double f
         }
         temp[i]='2';
         temp[i+1]='1';
-        temp[i+2]='\0';
-        temp[i+3]='.';
-        temp[i+4]='c';
-        temp[i+5]='s';
-        temp[i+6]='v';
-        filename=strcat(temp,".csv");
+        temp[i+2]='.';
+        temp[i+3]='c';
+        temp[i+4]='s';
+        temp[i+5]='v';
+        temp[i+6]='\0';
+        //filename=strcat(temp,"scsv");
         i=0;
-        while(filename[i]!='\0'){
-            if(filename[i]==' '){
-                filename[i]='_';
+        while(temp[i]!='\0'){
+            if(temp[i]==' '){
+                temp[i]='_';
             }
             i++;
         }
         char f_path[30];
-        snprintf(f_path, 30, "../log/%s", filename);
+        snprintf(f_path, 30, "../log/%s", temp);
         fp=fopen(f_path,"w");
     }
     else{
