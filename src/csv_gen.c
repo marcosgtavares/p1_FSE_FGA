a0041458fd534ac2c84ff720398bb68b759d0ee6
Marcos Gabriel Tavares <marcosgabriel.ro@hotmail.com>
2021-03-18 23:43:28 -0400

    csv generator fixed

--- src/csv_gen.c
+++ src/csv_gen.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 #include <string.h>
-#include "csv_gen.h"
+#include "../inc/csv_gen.h"
 
 FILE *fp;
 void create_csv(char *d_h, double ti, double te, double tr, double res, double fan){
     if(ti==0 && te==0 && tr==0 && res==0 && fan==0){
-        char *filename;
-        filename=strcat(d_h,".csv");
+        char *filename, temp[23];
         int i=0;
+        while(d_h[i+9]!='\0'){
+            temp[i]=d_h[i+4];
+            i++;
+        }
+        temp[i]='2';
+        temp[i+1]='1';
+        temp[i+2]='\0';
+        temp[i+3]='.';
+        temp[i+4]='c';
+        temp[i+5]='s';
+        temp[i+6]='v';
+        filename=strcat(temp,".csv");
+        i=0;
         while(filename[i]!='\0'){
             if(filename[i]==' '){
                 filename[i]='_';
